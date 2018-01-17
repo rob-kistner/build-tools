8cbf745b8c0f7ea097a212c101cd4bc1208b2d5d
rob-kistner <>
2018-01-17 12:41:13 -0600

    initial commit, master branch is current working build

---
+++ src/app/imported-component.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ImportedComponent extends React.Component
+{
+    render() {
+        return (
+            <div>
+                <p>Output from imported component</p>
+            </div>
+        )
+    }
+}
+
+export default ImportedComponent;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ImportedComponent from './app/imported-component';
+
+
+class App extends React.Component
+{
+    render() {
+        return (
+            <div>
+                <h2>React / Webpack Tooling</h2>
+                <hr/>
+                <ImportedComponent />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#app')
+);
